9e9b21933d0a3d61985e1b9bf90a107036ca4888
RjLlebenchLupo <2013000288@my.xu.edu.ph>
2026-04-29 12:10:30 +0800

    Implement dotenv for security purposes and updated middleware order

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth'); 
@@ -5,9 +7,10 @@ const roomRoutes = require('./routes/room');
 const cors = require('cors');
 
 // Connect to the Docker MongoDB database
-mongoose.connect('mongodb://database:27017/dormisync')
+// Replace the hardcoded string with process.env.MONGO_URI
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB is successfully connected!'))
-  .catch((err) => console.log('Database connection error: ', err));
+  .catch(err => console.log('Database connection error: ', err));
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -15,7 +18,8 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/rooms', roomRoutes); 
 
-const PORT = 5000;
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Real server is awake and listening on port ${PORT}`);
 });
